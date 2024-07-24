c6ac4cc98b7a84af7b181be5ac872fe78b864865
TxИ <95131051+nit27@users.noreply.github.com>
2024-07-24 21:35:38 +0700

    update user-overrides.js with simpler tweaks

--- user-overrides.js
+++ user-overrides.js
@@ -1,92 +1,57 @@
-// Firefox hardening using preferences
-user_pref("app.normandy.first_run", false);
-user_pref("app.shield.optoutstudies.enabled", false);
-user_pref("app.update.auto", false);
-user_pref("browser.contentblocking.category", "custom");
-user_pref("browser.download.useDownloadDir", false);
-user_pref("browser.formfill.enable", false);
-user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
-user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
-user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
-user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
-user_pref("browser.search.suggest.enabled", false);
+// Set default search engine: DuckDuckGo
 user_pref("browser.urlbar.placeholderName", "DuckDuckGo");
-user_pref("datareporting.healthreport.uploadEnabled", false);
+
+// Set default permissions
+ // * Location, Camera, Microphone, VR
+ // * 0=always ask (default), 1=allow, 2=block.
+user_pref("permissions.default.geo", 2);
+user_pref("permissions.default.camera", 2);
+user_pref("permissions.default.microphone", 0);
+user_pref("permissions.default.desktop-notification", 0);
+user_pref("permissions.default.xr", 2); // Virtual Reality.
+
+// Fingerprints
+user_pref("privacy.resistFingerprinting.letterboxing", false); // Disable letterbox (optional).
+user_pref("privacy.resistFingerprinting", true); // Enable RFP in NormalMode.
+user_pref("privacy.resistFingerprinting.pbmode", true); // Enable RFP in PBMode.
+
+ // Disable spellchecker
+user_pref("layout.spellcheckDefault", 1); // Set to `0` to disable spellcheck (optional).
+
+// Custom DNS over HTTPS
+user_pref("network.trr.custom_uri", "https://doh.mullvad.net/dns-query"); // Choose custom server here https://mullvad.net/en/help/dns-over-https-and-dns-over-tls.
+user_pref("network.trr.mode", 2); // Set to `5` to disable Mullvad DNS over HTTPS.
+user_pref("network.trr.uri", "https://doh.mullvad.net/dns-query");
+user_pref("network.dns.disableIPv6", true); // Disable IPv6.
 user_pref("doh-rollout.disable-heuristics", true);
-user_pref("dom.forms.autocomplete.formautofill", true);
+
+// Use HTTPS ONLY MODE
 user_pref("dom.security.https_only_mode_ever_enabled", true);
-user_pref("dom.security.https_only_mode", true);
+
+// Custom Enhance Tracking Protection
+user_pref("browser.contentblocking.category", "Custom"); //Replace for "Strict" if don't use a good VPN or UBO.
+
+// Disable autofill address & credit card
 user_pref("extensions.formautofill.addresses.enabled", false);
 user_pref("extensions.formautofill.creditCards.enabled", false);
-user_pref("extensions.pocket.enabled", false);
-user_pref("identity.fxaccounts.enabled", false);
-user_pref("layout.spellcheckDefault", 1); // Used to disable spellchecker. Set to `1` to enable spellcheck.
-user_pref("media.peerconnection.enabled", false); // Used to disable WebRTC (mitigating WebRTC leaks)… set to `true` to enable WebRTC
-user_pref("network.cookie.cookieBehavior", 1);
-user_pref("network.cookie.lifetimePolicy", 0); // Used to delete cookies when Firefox is closed. Set to `0` to enable default cookie persistence
-user_pref("network.proxy.socks_remote_dns", true);
-user_pref("privacy.clearSiteData.cookiesAndStorage", true); // keep false until it respects "allow" site exceptions
-user_pref("network.trr.custom_uri", "https://doh.mullvad.net/dns-query");
-user_pref("network.trr.mode", 3); // Used to enable Mullvad DNS over HTTPS. Set to `5` to disable Mullvad DNS over HTTPS
-user_pref("network.trr.uri", "https://doh.mullvad.net/dns-query");
-user_pref("places.history.enabled", false);
-user_pref("privacy.donottrackheader.enabled", true);
-user_pref("privacy.history.custom", true);
-user_pref("privacy.sanitize.sanitizeOnShutdown", false); // Used to delete cookies and site data when Firefox is closed. Set to `false` to enable cookie and site data persistence
-user_pref("privacy.trackingprotection.enabled", true);
-user_pref("privacy.trackingprotection.socialtracking.enabled", true);
-user_pref("signon.management.page.breach-alerts.enabled", false);
-user_pref("signon.rememberSignons", false);
-// Firefox hardening using about:config
-user_pref("accessibility.force_disabled", 1);
-user_pref("app.normandy.api_url", "");
-user_pref("app.normandy.enabled", false);
-user_pref("app.shield.optoutstudies.enabled", false);
+
+// Disable built-in bullshit features
+user_pref("extensions.pocket.enabled", false); // Mozilla Pocket.
+user_pref("identity.fxaccounts.enabled", false); // Unknow service (correct me if you know).
+user_pref("extensions.fxmonitor.enabled", false); // Unknow service (correct me if you know).
+user_pref("browser.messaging-system.whatsNewPanel.enabled", false); // "What's new" gift icon from toolbar and main menu.
+user_pref("dom.forms.autocomplete.formautofill", false); // Disable autofill.
 user_pref("beacon.enabled", false);
-user_pref("browser.pagethumbnails.capturing_disabled", true);
-user_pref("browser.ping-centre.telemetry", false);
-user_pref("browser.places.speculativeConnect.enabled", false);
-user_pref("browser.sessionstore.privacy_level", 2);
-user_pref("browser.ssl_override_behavior", 1);
-user_pref("browser.tabs.crashReporting.sendReport", false);
-user_pref("browser.uitour.enabled", false);
-user_pref("browser.uitour.url", "");
-user_pref("browser.urlbar.speculativeConnect.enabled", false);
-user_pref("browser.urlbar.suggest.quicksuggest.nonsponsored", false);
-user_pref("browser.urlbar.suggest.quicksuggest.sponsored", false);
-user_pref("browser.urlbar.trimURLs", false);
-user_pref("browser.xul.error_pages.expert_bad_cert", true);
-user_pref("captivedetect.canonicalURL", "");
-user_pref("datareporting.policy.dataSubmissionEnabled", false);
-user_pref("dom.security.https_only_mode_send_http_background_request", false);
-user_pref("extensions.getAddons.showPane", false);
-user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
-user_pref("geo.provider.use_corelocation", false);
-user_pref("network.auth.subresource-http-auth-allow", 1);
-user_pref("network.captive-portal-service.enabled", false);
-user_pref("network.connectivity-service.enabled", false);
-user_pref("network.dns.disableIPv6", true);
-user_pref("network.dns.disablePrefetch", true);
-user_pref("network.http.speculative-parallel-limit", 0);
-user_pref("network.predictor.enabled", false);
-user_pref("network.prefetch-next", false);
-user_pref("pdfjs.enableScripting", false);
-user_pref("privacy.userContext.enabled", true);
-user_pref("privacy.userContext.ui.enabled", true);
-user_pref("security.cert_pinning.enforcement_level", 2);
-user_pref("security.mixed_content.block_display_content", true);
-user_pref("security.OCSP.require", true);
-user_pref("security.pki.crlite_mode", 2);
-user_pref("security.pki.sha1_enforcement_level", 1);
-user_pref("security.remote_settings.crlite_filters.enabled", true);
-user_pref("security.ssl.require_safe_negotiation", true);
-user_pref("security.ssl.treat_unsafe_negotiation_as_broken", true);
-user_pref("security.tls.enable_0rtt_data", false);
-user_pref("toolkit.coverage.endpoint.base", "");
-user_pref("toolkit.coverage.opt-out", true);
-user_pref("toolkit.telemetry.coverage.opt-out", true);
-// Firefox fingerprinting hardening using about:config
-user_pref("privacy.resistFingerprinting", true); // Used to help resist fingerprinting but breaks dark mode and screenshots (among other features). Set to `true` for increased privacy
-user_pref("privacy.resistFingerprinting.block_mozAddonManager", true);
-user_pref("privacy.resistFingerprinting.letterboxing", false); // Used to help resist fingerprinting. Set to `false` to disable letterboxing
-user_pref("webgl.disabled", true);
+
+// Password Manager. I recommend KeePass or KeePassXC instead
+user_pref("signon.rememberSignons", false); // Do not save logins and passwords for sites.
+user_pref("signon.management.page.breach-alerts.enabled", false); // Hide alerts about passwords for breached sites.
+
+// Disable WebRTC (WebGL was already disable from user.js)
+user_pref("media.peerconnection.enabled", false);
+
+// Protect your private IP even in TRUSTED scenarios after you grant device access
+user_pref("media.peerconnection.ice.no_host", true); // May result in breakage on video-conferencing platforms.
+
+// Disable GMP (Gecko Media Plugins)
+user_pref("media.gmp-provider.enabled", false);
